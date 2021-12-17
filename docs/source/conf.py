7dc5315353058b73933d3989b516d1389902f533
Dima Nikiforov <vnikiforov@berkeley.edu>
2021-12-17 11:48:43 -0800

    update conf.py with project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Robot Co-Sim'
+copyright = '2021, Chris Dong and Dima Nikiforov'
+author = 'Chris Dong and Dima Nikiforov'
 
 release = '0.1'
 version = '0.1.0'
